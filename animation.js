016dafb5a177bab2d91a50f1586dcfec1775e23f
xb <xboooox123@gmail.com>
2019-07-26 04:44:10 +0500

    Balls

--- animation.js
+++ animation.js
@@ -44,17 +44,17 @@ var timer;
 function start() {
 	// draw border
 	context.beginPath();
-    context.strokeStyle = "Black";
-    context.lineWidth = 3;
-    context.strokeRect(5, 5, 900, 900);
-    
-    // draw background
-    context.fillStyle = "#FFA07A";
-    context.fillRect(7, 7, 896, 896);
-    context.closePath();
-    
-    var radios = document.getElementsByName('numberOfBalls');
-    for (var num = 0; num < radios.length; num++) {
+	context.strokeStyle = "Black";
+	context.lineWidth = 3;
+	context.strokeRect(5, 5, 900, 900);
+
+	// draw background
+	context.fillStyle = "#FFA07A";
+	context.fillRect(7, 7, 896, 896);
+	context.closePath();
+
+	var radios = document.getElementsByName('numberOfBalls');
+	for (var num = 0; num < radios.length; num++) {
     	if (radios[num].checked) {
     		var tmp = numberOfBalls;
     		numberOfBalls = num + 1;
@@ -67,14 +67,14 @@ function start() {
 					} while (overlayBalls(i) != -1);
 			}
     	}
-    }
-    
-    clearInterval(timer); // stop last timer
-    timer = setInterval('animation();', 10); // delay = 10 milliseconds 
+	}
+
+	clearInterval(timer); // stop last timer
+	timer = setInterval('animation();', 10); // delay = 10 milliseconds 
 }
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function animation() {
@@ -86,14 +86,14 @@ function animation() {
 
 		if (overlayBalls(num) != -1) {
 				balls[num].previousPosition();	
-				
+		
 				balls[num].deltaX *= -1;
 				balls[num].deltaY *= -1;
 				
 				balls[num].collisionsWithEdges(8, 902);
 				balls[num].nextPosition();
 		}
-	    
+    
 	    drawBall(num);
 	}
 }
@@ -101,7 +101,7 @@ function animation() {
 function overlayBalls(num) {
 	for (var i = 0; i < numberOfBalls; i++) {
 		if (i == num) continue;
-		
+
 		// distance between centers of the balls < sum their radiuses	
 		if (Math.sqrt(Math.pow(balls[num].x - balls[i].x, 2) + Math.pow(balls[num].y - balls[i].y, 2)) < balls[num].radius + balls[i].radius)
 			return i;
@@ -112,22 +112,22 @@ function overlayBalls(num) {
 
 function eraseBall(num) {
 	context.beginPath();
-    context.arc(balls[num].x, balls[num].y, balls[num].radius, 0, 2 * Math.PI); // (x-center, y-center, radius, startAngle, endAngle [, counterclockwise])
+	context.arc(balls[num].x, balls[num].y, balls[num].radius, 0, 2 * Math.PI); // (x-center, y-center, radius, startAngle, endAngle [, counterclockwise])
 	context.fillStyle = "#FFA07A";
 	context.fill();
-    context.strokeStyle = "#FFA07A";
-    context.lineWidth = 3;
-    context.stroke();
-    context.closePath();	
+	context.strokeStyle = "#FFA07A";
+	context.lineWidth = 3;
+	context.stroke();
+	context.closePath();	
 }
 
 function drawBall(num) {
 	context.beginPath();
-    context.arc(balls[num].x, balls[num].y, balls[num].radius, 0, 2 * Math.PI);
-    context.fillStyle = balls[num].color;
-    context.fill();
-    context.strokeStyle = "Black";
-    context.lineWidth = 1;
-    context.stroke();
-    context.closePath();	
+	context.arc(balls[num].x, balls[num].y, balls[num].radius, 0, 2 * Math.PI);
+	context.fillStyle = balls[num].color;
+	context.fill();
+	context.strokeStyle = "Black";
+	context.lineWidth = 1;
+	context.stroke();
+	context.closePath();	
 }
